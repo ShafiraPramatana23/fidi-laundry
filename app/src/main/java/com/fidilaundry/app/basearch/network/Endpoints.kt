ce888c6aa6e3a56aec2b6fc75af3b736ee2ec52d
ShafiraPramatana23 <firapramatana03@gmail.com>
2022-10-15 18:03:30 +0700

    first commit

---
+++ app/src/main/java/com/fidilaundry/app/basearch/network/Endpoints.kt
@@ -0,0 +1,10 @@
+package com.fidilaundry.app.basearch.network
+
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.*
+
+interface Endpoints {
+
+}
